201420f54615cc0dc3b6c707cfd6287c4771dc74
SoyEdwinCabrera <edwinleeroth@gmail.com>
2024-07-09 00:21:50 -0500

    App ChatGPT

--- hello_reflex/hello_reflex.py
+++ hello_reflex/hello_reflex.py
@@ -1,31 +1,53 @@
 import reflex as rx
+from hello_reflex import style
+from hello_reflex.state import State 
 
 
-class State(rx.State):
-    count: int = 0
 
-    def increment(self):
-        self.count += 1
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(answer, style=style.answer_style),
+            text_align="left",
+        ),
+        margin_y="1em",
+    )
 
-    def decrement(self):
-        self.count -= 1
+def chat() -> rx.Component:
+    return rx.box(
+        rx.foreach(
+            State.chat_history,
+            lambda messages: qa(messages[0], messages[1]),
+        )
+    )
 
 
-def index():
-    return rx.vstack(
-        rx.hstack(
-            rx.button(
-                "Decrement",
-                color_scheme="ruby",
-                on_click=State.decrement,
-            ),
-            rx.heading(State.count, font_size="2em"),
-            rx.button(
-                "Increment",
-                color_scheme="grass",
-                on_click=State.increment,
-            ),
-            spacing="4",
+
+def action_bar() -> rx.Component:
+    return rx.hstack(
+        rx.input(
+            placeholder="Ask a question",
+            on_change=State.set_question,
+            style=style.input_style,
+        ),
+        rx.button(
+            "Ask",
+            on_click=State.answer,
+            style=style.button_style,
+        ),
+    )
+
+
+def index() -> rx.Component:
+    return rx.center(
+        rx.vstack(
+            chat(),
+            action_bar(),
+            align="center",
         )
     )
 
